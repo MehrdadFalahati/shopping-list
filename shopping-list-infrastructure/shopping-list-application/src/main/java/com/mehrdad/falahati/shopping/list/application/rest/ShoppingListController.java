29ffaa4e2ddb2d99c157d586f801a7efdc08be2b
MehrdadFalahati <m.falahati2022@gmail.com>
2023-08-18 15:21:34 +0330

    feat: created infrastructure application rest api.

---
+++ shopping-list-infrastructure/shopping-list-application/src/main/java/com/mehrdad/falahati/shopping/list/application/rest/ShoppingListController.java
@@ -0,0 +1,51 @@
+package com.mehrdad.falahati.shopping.list.application.rest;
+
+import com.mehrdad.falahati.shopping.list.application.service.ShoppingListApplicationService;
+import com.mehrdad.falahati.shopping.list.application.service.dto.CreateShoppingItemCommand;
+import com.mehrdad.falahati.shopping.list.application.service.dto.GetTotalPriceResponse;
+import com.mehrdad.falahati.shopping.list.application.service.dto.ShoppingListIdResponse;
+import com.mehrdad.falahati.shopping.list.application.service.dto.TotalPriceQuery;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.UUID;
+
+import static com.mehrdad.falahati.shopping.list.application.rest.ShoppingListController.BASE_URI;
+
+@RestController
+@RequestMapping(BASE_URI)
+@RequiredArgsConstructor
+public class ShoppingListController {
+    protected static final String BASE_URI = "/api/v1/shopping-lists";
+
+    private final ShoppingListApplicationService shoppingListService;
+
+    @PostMapping("/")
+    public ResponseEntity<?> createShoppingList(UriComponentsBuilder uriComponentsBuilder) {
+        UriComponents uriComponents = uriComponentsBuilder
+                .path(BASE_URI + "/{shoppingListId}")
+                .buildAndExpand(
+                        shoppingListService.createShoppingList()
+                );
+
+        return ResponseEntity.created(uriComponents.toUri()).build();
+    }
+
+    @PutMapping("/{shoppingListId}/items")
+    public ResponseEntity<?> addItemToTheShoppingList(UriComponentsBuilder uriComponentsBuilder, @RequestBody CreateShoppingItemCommand command) {
+        UriComponents uriComponents = uriComponentsBuilder
+                .path(BASE_URI + "/" + command.shoppingListId() + "/items/{itemId}")
+                .buildAndExpand(
+                        shoppingListService.addItemToTheShoppingList(command)
+                );
+        return ResponseEntity.created(uriComponents.toUri()).build();
+    }
+
+    @GetMapping("/{shoppingListId}/total-price")
+    public ResponseEntity<GetTotalPriceResponse> getTotalPrice(@PathVariable UUID shoppingListId) {
+        return ResponseEntity.ok().body(shoppingListService.getTotalPrice(new TotalPriceQuery(shoppingListId)));
+    }
+}
